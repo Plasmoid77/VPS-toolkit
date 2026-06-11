e873a87173f23d0e60422e7ffd7af68d7082d0a3
Plasmoid28 <plasmoid28@noreply.codeberg.org>
2026-06-12 01:38:57 +0200

    Add scripts/ufw-disable-ping.sh

---
+++ scripts/ufw-disable-ping.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# Запрещаем входящий IPv4 ping, не трогая остальные ICMP-типы
+sudo sed -i 's/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/' /etc/ufw/before.rules
+
+# Запрещаем входящий IPv6 ping, не ломая остальные ICMPv6-типы
+sudo sed -i 's/-A ufw6-before-input -p icmpv6 --icmpv6-type echo-request -j ACCEPT/-A ufw6-before-input -p icmpv6 --icmpv6-type echo-request -j DROP/' /etc/ufw/before6.rules
+
+# Перезагружаем UFW без полного disable/enable
+sudo ufw reload
+
+# Проверяем статус UFW
+sudo ufw status numbered

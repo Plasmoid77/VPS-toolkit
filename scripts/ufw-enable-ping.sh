0bef7f7f0f47b24d988c46d329533ce4436565d4
Plasmoid77 <github@to.plazma.cc>
2026-09-01 15:47:02 +0000

    Update ufw-enable-ping.sh

--- scripts/ufw-enable-ping.sh
+++ scripts/ufw-enable-ping.sh
@@ -5,13 +5,10 @@ set -e
 sudo sed -i 's/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/' /etc/ufw/before.rules
 
 # Разрешаем входящий IPv6 ping обратно
-sudo sed -i 's/-A ufw6-before-input -p icmpv6 --icmpv6-type echo-request -j DROP/-A ufw6-before-input -p icmpv6 --icmpv6-type echo-request -j ACCEPT/' /etc/ufw/before6.rules
+sudo sed -i 's/-A ufw6-before-input -p ipv6-icmp --icmpv6-type echo-request -j DROP/-A ufw6-before-input -p ipv6-icmp --icmpv6-type echo-request -j ACCEPT/' /etc/ufw/before6.rules
 
-# Перезагружаем UFW без полного disable/enable
+# Перезагружаем UFW
 sudo ufw reload
-
-# Проверяем статус UFW
-sudo ufw status numbered
 
 echo
 printf '\033[1;32m%s\033[0m\n' "============================================================"
